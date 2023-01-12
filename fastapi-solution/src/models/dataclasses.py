2c6882b1840eb117c3f1d91ffd0d748ab4e916ac
Полина <PGVasilyeva@sberbank.ru>
2023-01-12 16:44:54 +0300

    Убрала networks из компоуза убрала закомментированные строки из гитигнора Объединила 2 датакласса фильма в один и переименовала Filmwork в Show

--- fastapi-solution/src/models/dataclasses.py
+++ fastapi-solution/src/models/dataclasses.py
@@ -5,30 +5,27 @@ from pydantic import UUID4, Field
 from base import BaseModel
 
 
-class Filmwork(BaseModel):
-    id: UUID4
-    title: str
-    imdb_rating: float
-
-
 class Genre(BaseModel):
     id: UUID4
     name: str
 
 
-class PersonFilmwork(BaseModel):
+class PersonShow(BaseModel):
     id: UUID4 = Field(alias="uuid")
     name: str = Field(alias="full_name")
 
 
-class FullFilm(Filmwork):
+class Show(BaseModel):
+    id: UUID4
+    title: str
+    imdb_rating: float
     description: Optional[str] = None
     genre: List[Genre]
-    actors: Optional[List[PersonFilmwork]] = None
-    writers: Optional[List[PersonFilmwork]] = Field(default_factory=list)
-    directors: Optional[List[PersonFilmwork]] = Field(default_factory=list)
+    actors: Optional[List[PersonShow]] = None
+    writers: Optional[List[PersonShow]] = Field(default_factory=list)
+    directors: Optional[List[PersonShow]] = Field(default_factory=list)
 
 
-class Person(PersonFilmwork):
+class Person(PersonShow):
     role: List[str]
     film_ids: List[UUID4]
