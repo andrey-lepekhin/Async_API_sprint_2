f0a0852825f8f32593012a823139d3bcf0688936
Полина <PGVasilyeva@sberbank.ru>
2023-01-27 21:48:18 +0300

    тесты для жанров

--- tests/functional/src/test_cache.py
+++ tests/functional/src/test_cache.py
@@ -1,8 +1,4 @@
 import asyncio
-import uuid
-from http import HTTPStatus
-from time import sleep
-
 import pytest
 
 from tests.functional.settings import test_settings
@@ -23,39 +19,32 @@ async def test_shows_show_id_cache(aiohttp_get, es_async_client, es_with_fresh_i
     assert response.body['id'] == show_id
 
 
-@pytest.mark.parametrize(
-    'person_id',
-    [
-        '6e429cff-c8a2-4d17-8b12-6532a8a1ac9b',
-        'bbdbad95-f08b-4e12-ba35-92b89c9251f8',
-        'efa1894b-0384-4164-b55d-424980142c28'
-     ]
-)
-async def test_persons_cache_pass(person_id, aiohttp_get, es_async_client, es_with_fresh_indexes) -> None:
-    endpoint = f'persons/{person_id}'
+async def test_genres_cache_pass(aiohttp_get, es_async_client, es_with_fresh_indexes) -> None:
+    genre_id = '526769d7-df18-4661-9aa6-49ed24e9dfd8'
+    endpoint = f'genres/{genre_id}'
     response = await aiohttp_get(endpoint)
-    assert response.body['id'] == person_id
+    assert response.body['id'] == genre_id
 
-    await es_async_client.delete(index=test_settings.person_index_name, id=person_id)
+    await es_async_client.delete(index=test_settings.genre_index_name, id=genre_id)
 
     response = await aiohttp_get(endpoint)
-    assert response.body['id'] == person_id
+    assert response.body['id'] == genre_id
 
 
-async def test_persons_cache_expiration(
+async def test_genres_cache_expiration(
         aiohttp_get, es_async_client, es_with_fresh_indexes
 ) -> None:
-    person_id = '6e429cff-c8a2-4d17-8b12-6532a8a1ac9b'
-    endpoint = f'persons/{person_id}'
+    genre_id = '5373d043-3f41-4ea8-9947-4b746c601bbd'
+    endpoint = f'genres/{genre_id}'
     response = await aiohttp_get(endpoint)
-    assert response.body['id'] == person_id
+    assert response.body['id'] == genre_id
 
-    await es_async_client.delete(index=test_settings.person_index_name, id=person_id)
+    await es_async_client.delete(index=test_settings.genre_index_name, id=genre_id)
     await asyncio.sleep(5)
 
     with pytest.raises(KeyError):
         response = await aiohttp_get(endpoint)
-        assert response.body['id'] == person_id
+        assert response.body['id'] == genre_id
 
 #TODO: someday: test cache invalidation
 #TODO: in tasks 5, 6, 7, 8: add cache testing for all endpoints.
